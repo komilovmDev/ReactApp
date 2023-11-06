c73d7efe903cee52c5bb4a904ff29fe1b33a1e56
Muhammad Komilov <120216920+komilovmDev@users.noreply.github.com>
2023-11-06 20:41:53 +0300

    user page dropdown add card

--- my-app/src/pages/UserPage/UserCards.jsx
+++ my-app/src/pages/UserPage/UserCards.jsx
@@ -5,6 +5,7 @@ import { AiOutlineDelete } from 'react-icons/ai'
 import BasicModal from '../../companents/BasicModal/Modal'
 import { BsCheckLg } from 'react-icons/bs'
 import { BiCheckboxMinus } from 'react-icons/bi'
+import {GoKebabHorizontal} from 'react-icons/go'
 import Button from '@mui/material/Button';
 import { useRef } from 'react'
 
@@ -40,29 +41,48 @@ export default function UserCards({ item }) {
         window.location.reload()
     }
 
-    
+    // 
+
+    const redCardAdd = async (id) => {
+        const response = await axios.post(`https://manager.zafarr.uz/to/${id}/tugatilmagan/`, null,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Token ${token}`,
+                },
+            }
+        )
+        window.location.reload()
+    }
+
+    const GreenCardAdd = async (id) => {
+        const response = await axios.post(`https://manager.zafarr.uz/to/${id}/bajarilganga/`, null,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Token ${token}`,
+                },
+            }
+        )
+        window.location.reload()
+    }
+
+
+
     const boardValue = useRef()
 
     return (
         <div className="UserCardBox" key={item.id}>
             <div className="UserCard">
                 <div className="boximgtype">
-                    {/* <div className="modal">
-                        <Button style={{ minWidth: 'auto' }} variant="text"><BiCheckboxMinus color='red' size={'30px'} /></Button>
-                        <Button style={{ minWidth: 'auto' }} variant="text"><BsCheckLg color='green' size={'30px'} /></Button>
-                        <BasicModal main={<button className='saveModal' onClick={deleteBoard}>Delete</button>} text={'Delete Board ?'} btn={<AiOutlineDelete color='red' size={'25px'} />} />
-                        <BasicModal
-                            element={<input onKeyPress={(e) => {
-                                if (e.key === "Enter") {
-                                    renameBoard(item.id, e.target.value)
-                                }
-                            }} className='modalIn' ref={boardValue} placeholder='Board Name' />}
-                            main={<button className='saveModal' onClick={(e) => {
-                                renameBoard(item.id, boardValue.current.value)
-                            }}>ok</button>} text={'Board rename'}
-                            btn={<BiPencil color='black' size={'25px'} />}
-                        />
-                    </div> */}
+                    <div className="dropdown">
+                        <button className="dropbtn"><GoKebabHorizontal /></button>
+                        <div className="dropdown-content" >
+                            <Button onClick={() => redCardAdd(item.id)} style={{ minWidth: 'auto' }} variant="text"><BiCheckboxMinus color='red' size={'30px'} /></Button>
+
+                            <Button onClick={() => GreenCardAdd(item.id)} style={{ minWidth: 'auto' }} variant="text"><BsCheckLg color='green' size={'30px'} /></Button>
+                        </div>
+                    </div>
                 </div>
                 <Link to={`/UserTaskInfo/${item.id}`}>
                     <div className="Teaxt">
