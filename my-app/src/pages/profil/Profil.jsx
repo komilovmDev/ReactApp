5ae871e0b5fd2601ab78b0f9fc55b0e96794be42
K A I N <73824592+Dark-dka@users.noreply.github.com>
2023-12-13 01:23:13 +0500

    scsdad

--- my-app/src/pages/profil/Profil.jsx
+++ my-app/src/pages/profil/Profil.jsx
@@ -1,22 +1,24 @@
 import { useState } from 'react'
 import './profil.css'
 
-export default function Profil({className , setClassName}) {
+export default function Profil({ className, setClassName }) {
 
     return (
         <div className={className}>
             <div className="profilInfosBox">
                 <img src="https://manager.zafarr.uz/media/profile/2023/11/30/IMG_1260.JPG" alt="" />
                 <div className="profilInfosBox__userInfos">
-                    <h3 className="username">Muhammad Yusupov</h3>
-                    <p className="kaspInfo">Frontend Devloper in Ranch</p>
+                    <div className='ProfilTextModal'>
+                        <h3 className="username">Muhammad Yusupov</h3>
+                        <p className="kaspInfo">Frontend Devloper in Ranch</p>
+                    </div>
                     <div className="btns">
                         <button>Admin</button>
                         <button>Delete User</button>
                         <button onClick={() => setClassName('close')}>Close</button>
                     </div>
                 </div>
-            </div>                           
+            </div>
         </div>
     )
 }
