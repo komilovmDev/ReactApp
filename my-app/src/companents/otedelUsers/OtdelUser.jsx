897edf574c96176278c63e22fd4bc40de3a495e5
K A I N <73824592+Dark-dka@users.noreply.github.com>
2023-12-16 11:00:15 +0500

    dawdsad

--- my-app/src/companents/otedelUsers/OtdelUser.jsx
+++ my-app/src/companents/otedelUsers/OtdelUser.jsx
@@ -8,7 +8,7 @@ export default function OtdelUser({ className, setClassName, item }) {
     return (
         <div className={className}>
             <div className="profilInfosBox rel">
-                <h2 className='OtedelName'>IT BO'LIMI</h2>
+                <h2 className='OtedelName'>IT bo'limi</h2>
                 <div className="userS">
                     {
                         item.users.map(data => (
@@ -19,10 +19,10 @@ export default function OtdelUser({ className, setClassName, item }) {
                             </div>
                         ))
                     }
-                    <button className='plusOtdel'><FaPlus /></button>
                 </div>
                 <div className="profilInfosBox__userInfos">
                     <div className="btns">
+                        <button className='plusOtdel'><FaPlus /></button>
                         <button className='closeBtn' onClick={() => setClassName('close')}>Close</button>
                     </div>
                 </div>
